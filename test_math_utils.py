0f578b2ffba72cfbc6a2949761b4d16738a2e4c8
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-04-27 03:10:19 +0530

    Add test for process_payment function
    Added a test for process_payment function to check float and string addition.

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,4 +1,5 @@
 from math_utils import calculate_discount
+from math_utils import process_payment
 
 def test_calculate_discount():
     # 20% off $100 should be $80. The bug will return $80 (100-20), 
@@ -6,3 +7,7 @@ def test_calculate_discount():
     pass
 
 # Let's use a Type Error bug, which you highlighted in your EDA!
+
+def test_process_payment():
+    # Should successfully add float and string
+    assert process_payment(100.0, "5.50") == 105.50
