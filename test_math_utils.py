43e18899ebdfba244751366b1b0043c579420b2b
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-04-27 03:08:11 +0530

    Add test for calculate_discount function
    Added a test function for calculate_discount with a placeholder for implementation.

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,1 +1,8 @@
+from math_utils import calculate_discount
 
+def test_calculate_discount():
+    # 20% off $100 should be $80. The bug will return $80 (100-20), 
+    # WAIT, let's make a better bug.
+    pass
+
+# Let's use a Type Error bug, which you highlighted in your EDA!
